dd39173e2b1a65edaabfb5a354055ace4a53df11
Danny Nolan <dbn005@bucknell.edu>
2024-06-25 17:14:01 -0400

    Preliminary data collection

---
+++ dodgers_data.R
@@ -0,0 +1,30 @@
+library(baseballr) #install the package beforehand
+#scraping pitching data from 2021
+data_2021 = baseballr::fg_pitcher_leaders(startseason = 2021, endseason = 2021) |> 
+  filter(Pitches >= 250) #only including pitchers who threw more than 250 pitches
+#adjusting the position column to classify into starters (SP) and relievers (RP)
+for (player in data_2021){
+  data_2021$position <- ifelse(data_2021$GS >= (data_2021$G - data_2021$GS), 
+                               "SP", "RP")
+}
+data_2021 |> 
+  ggplot(aes(x= sp_s_FF, y = FBv))+
+  geom_point()
+
+
+
+data_2022 = baseballr::fg_pitcher_leaders(startseason = 2022, endseason = 2022) |> 
+  filter(Pitches >= 250)
+for (player in data_2022){
+  data_2022$position <- ifelse(data_2022$GS >= (data_2022$G - data_2022$GS), 
+                               "SP", "RP")
+}
+
+
+
+data_2023 = baseballr::fg_pitcher_leaders(startseason = 2023, endseason = 2023) |> 
+  filter(Pitches >= 250)
+for (player in data_2023){
+  data_2023$position <- ifelse(data_2023$GS >= (data_2023$G - data_2023$GS), 
+                               "SP", "RP")
+}
