052ea85c28b68a3af80fb6f26ba312fd69c06273
Danny Nolan <dbn005@bucknell.edu>
2024-06-26 17:56:49 -0400

    creation of condensed data set for all 3 years.

--- dodgers_data.R
+++ dodgers_data.R
@@ -15,8 +15,8 @@ data_2021 |>
 
 
 
-data_2022 = baseballr::fg_pitcher_leaders(startseason = 2022, endseason = 2022) |> 
-  filter(Pitches >= 250)
+data_2022 = baseballr::fg_pitcher_leaders(startseason = 2022, endseason = 2022)
+data_2022 <- data_2022[which(data_2022$Pitches >= 250),]
 for (player in data_2022){
   data_2022$position <- ifelse(data_2022$GS >= (data_2022$G - data_2022$GS), 
                                "SP", "RP")
@@ -24,12 +24,57 @@ for (player in data_2022){
 
 
 
-data_2023 = baseballr::fg_pitcher_leaders(startseason = 2023, endseason = 2023) |> 
-  filter(Pitches >= 250)
+data_2023 = baseballr::fg_pitcher_leaders(startseason = 2023, endseason = 2023)
+data_2023 <- data_2023[which(data_2023$Pitches >= 250),]
 for (player in data_2023){
   data_2023$position <- ifelse(data_2023$GS >= (data_2023$G - data_2023$GS), 
                                "SP", "RP")
 }
+#creating condensed dataseet for 2021
+library(dplyr)
+cond_data_2021 <- data_2021 |> 
+  select(Season, Position, IP, Throws, xMLBAMID, PlayerNameRoute, `ERA-`, `K_BB+`, `HR_9+`, `WHIP+`,
+         `AVG+`, `FIP-`, `BABIP+`, RAR, WAR, `RA9-Wins`, `xFIP-`, WPA, RE24, REW,
+         FB_pct1, FBv, SL_pct,  SLv, CT_pct, CTv, CB_pct, CBv, CH_pct, CHv,
+         sp_s_FF, sp_s_SI, sp_s_SL, sp_s_FC, sp_s_CU, sp_s_CH, sp_s_FS, sp_s_KC,
+         sp_stuff, SIERA, KNv, KN_pct,`Soft_pct+`, `Med_pct+`, `Hard_pct+`, xERA, 
+         )
 
-
-test_2023 = baseballr::fg_pitch_leaders(startseason = 2021, endseason = 2023)
+#Creating condensed dataset for 2022
+cond_data_2022 <- data_2022 |> 
+  select(Season, Position, IP, Throws, xMLBAMID, PlayerNameRoute, `ERA-`, `K_BB+`, `HR_9+`, `WHIP+`,
+         `AVG+`, `FIP-`, `BABIP+`, RAR, WAR, `RA9-Wins`, `xFIP-`, WPA, RE24, REW,
+         FB_pct1, FBv, SL_pct,  SLv, CT_pct, CTv, CB_pct, CBv, CH_pct, CHv,
+         sp_s_FF, sp_s_SI, sp_s_SL, sp_s_FC, sp_s_CU, sp_s_CH, sp_s_FS, sp_s_KC,
+         sp_stuff, SIERA, KNv, KN_pct,`Soft_pct+`, `Med_pct+`, `Hard_pct+`, xERA, 
+  )
+#Creating condensed dataset for 2023
+cond_data_2023 <- data_2023 |> 
+  select(Season, Position, IP, Throws, xMLBAMID, PlayerNameRoute, `ERA-`, `K_BB+`, `HR_9+`, `WHIP+`,
+         `AVG+`, `FIP-`, `BABIP+`, RAR, WAR, `RA9-Wins`, `xFIP-`, WPA, RE24, REW,
+         FB_pct1, FBv, SL_pct,  SLv, CT_pct, CTv, CB_pct, CBv, CH_pct, CHv,
+         sp_s_FF, sp_s_SI, sp_s_SL, sp_s_FC, sp_s_CU, sp_s_CH, sp_s_FS, sp_s_KC,
+         sp_stuff, SIERA, KNv, KN_pct,`Soft_pct+`, `Med_pct+`, `Hard_pct+`, xERA, 
+  )
+#combining all three condensed datasets
+cond_data = rbind(cond_data_2021, cond_data_2022, cond_data_2023)
+  
+#Experimenting with visualizations
+cond_data |> 
+  ggplot(aes(x=sp_s_FF, y=sp_s_CH))+
+  geom_point()
+cond_data |> 
+  ggplot(aes(x=sp_s_FF))+
+  geom_histogram()  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
