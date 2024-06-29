3666c2189e72b5de3e2fd342ae28fb50e11afbb4
ezegabriel <chieloka.eze@centre.edu>
2024-06-29 14:33:28 -0400

    more viz

--- Eze/idea.R
+++ Eze/idea.R
@@ -35,7 +35,7 @@ for (player in data_2023){
 
 key_vars <- c("team_name", "Throws", "PlayerName", "Age", "W", "L", "ERA", "G", "GS", "SO", "IP", "Pitches", 
               "Balls", "Strikes", "FB_pct", 'FB_pct1', 'SL_pct', "FBv", "SL_pct", "SLv", "CT_pct", "CTv", "CB_pct", "CBv", "CH_pct", "CHv",
-              "sp_stuff", "sp_location", "sp_pitching", 'WAR', 'wFB')
+              "sp_stuff", "sp_location", 'K_9', "sp_pitching", 'WAR', 'wFB')
 cond_data_2021 <- data_2021 |> 
   dplyr::select(all_of(key_vars))
 
@@ -50,10 +50,10 @@ cond_data_2021 |>
   labs(title = "Distribution of ERA", x = "ERA", y = "Frequency")
 
 cond_data_2021 |> 
-  ggplot(aes(x = FBv, y = SO))+
-  geom_point(alpha = .5, color = 'red')+
+  ggplot(aes(x = FBv, y = K_9))+
+  geom_point(alpha = .45, color = 'red', size = .9)+
   geom_smooth(method = 'lm', color = 'blue', se = F)+
-  labs(title = "Fastball Velocity vs Strikeouts", x = "Fastball Velocity (mph)", y = "Strikeouts")
+  labs(title = "Fastball Velocity vs Strikeouts per 9 innings", x = "Fastball Velocity (mph)", y = "Strikeouts / 9 innings")
 
 # Boxplot of ERA by Pitching Hand (Throws)
 cond_data_2021 |> 
@@ -75,16 +75,16 @@ cond_data_2021 |>
 # Stuff+ vs Performance
 cond_data_2021 |> 
   ggplot(aes(sp_stuff, WAR))+
-  geom_point(alpha = .5)+
+  geom_point(alpha = .45, size = .9)+
   geom_smooth(method = 'lm')+
-  labs(title = "Stuff+ vs WAR", x = "Stuff+", y = "WAR")
+  labs(title = "WAR vs Stuff+", x = "Stuff+", y = "WAR")
 
 # Pitch Type vs Performance:
 cond_data_2021 |> 
   ggplot(aes(wFB, ERA))+
-  geom_point(alpha = .5)+
+  geom_point(alpha = .45, size = .9)+
   geom_smooth(method = 'lm')+
-  labs(title = "Fastball Value (wFB) vs ERA", x = "wFB", y = "ERA")
+  labs(title = "ERA vs Fastball Value (wFB)", x = "wFB", y = "ERA")
 
 
 library(ggcorrplot)
@@ -119,9 +119,118 @@ cond_data_2021 |>
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
+
+
+library(ggridges)
+library(patchwork)
+
+
+# Combine datasets for seasonal info --------------------------------------
+
+
+combined_data <- bind_rows(data_2021, data_2022, data_2023)
+
+
+
+# Filter relevant columns -------------------------------------------------
+
+
+
+# This approach ensures we retain rows that have data for any of the pitch types. 
+# If a row has data for a fastball (FB_pct1), but not for other pitches, it will still be included.
+
+filtered_data <- combined_data |> 
+  dplyr::select(Season, PlayerName, FB_pct1, SL_pct, CT_pct, CB_pct, CH_pct) |> 
+  filter(!is.na(FB_pct1) | !is.na(SL_pct) | !is.na(CT_pct) | !is.na(CB_pct) | !is.na(CH_pct))
+
+
+
+# Creating ridge plots ----------------------------------------------------
+
+
+create_ridge_plot <- function(data_set, pitch_col, pitch_name) {
+  ggplot(data_set, aes_string(x = pitch_col, y = "as.factor(Season)", fill = "as.factor(Season)")) +
+    geom_density_ridges(scale = 0.9, rel_min_height = 0.01) +
+    labs(
+      title = paste("Distribution of", pitch_name, "Across Seasons"),
+      x = paste(pitch_name, "Percentage"),
+      y = "Season",
+      fill = "Season"
+    ) +
+    theme_ridges() +
+    theme(legend.position = "none")
+    
+}
+
+
+
+
+
+# Use function to create plots for each pitch -----------------------------
+
+
+plot_fb <- create_ridge_plot(filtered_data, 'FB_pct1', "Fastball")
+plot_sl <- create_ridge_plot(filtered_data, 'SL_pct', 'Slider')
+plot_ct <- create_ridge_plot(filtered_data, 'CT_pct', 'Cutter')
+plot_cb <- create_ridge_plot(filtered_data, 'CB_pct', 'Curveball')
+plot_ch <- create_ridge_plot(filtered_data, 'CH_pct', 'Changeup')
+
+
+
+# Print relevant plot onto plots plane  -----------------------------------
+
+
+print(plot_fb)
+print(plot_sl)
+print(plot_ct)
+print(plot_cb)
+print(plot_ch)
+
+
+# pitch_data <- combined_data |> 
+#   dplyr::select(Season, FB_pct1, SL_pct, CT_pct, CB_pct, CH_pct) |> 
+#   pivot_longer(starts_with('FB_pct1') : starts_with('CH_pct'),
+#                names_to = 'Pitch_Type',
+#                values_to = 'Usage_percentage')
+# 
+# pitch_data |> 
+#   ggplot(aes(x = Usage_percentage, y = Pitch_Type, fill = factor(Season)))+
+#   geom_density_ridges(scale = 3, rel_min_height = .01)+
+#   labs(title = "Pitch Usage Distribution Over Seasons",
+#        x = "Usage Percentage",
+#        y = "Pitch Type",
+#        fill = "Season") +
+#   theme_ridges() +
+#   theme(legend.position = "top")
+# 
+# 
+# # Ridge plot for Fastball percentage over seasons
+# filtered_data |> 
+#   ggplot(aes(x = FB_pct, y = as.factor(Season), fill = as.factor(Season)))+
+#   geom_density_ridges(scale = .9, rel_min_height =  .01)+
+#   labs(
+#     title = 'Distribution of Fastball Percentage Across Seasons',
+#     x = "Fastball Percentage",
+#     y = "Season",
+#     fill = "Season"
+#   ) +
+#   theme_ridges() +
+#   theme(legend.position = "none")
+# 
+
+
+
+
 # Out of the myriads of variables which one is important?
 # EDA
+# runs above average to stuff+
 
 
+# ridge plots over time !boxplot
+  # a way to tally changes in arsenal
+
+# tree base approach to output potential run value
+# plots movements vs avg on each axis(pfx with dash)
+  # color by maybe slider for now
 # Limitation
   # In creating these metrics for pitch arsenal, stuff plus (sp) doesn't take into account the batter's action
